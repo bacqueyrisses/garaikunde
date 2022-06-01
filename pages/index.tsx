ce50bd1fa946dd5b18beba7be330fc979023904a
Enzo <96829831+bacqueyrisses@users.noreply.github.com>
2022-06-01 15:39:13 +0200

    gtag install

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from './components/Header'
 
 const Home: NextPage = () => {
   return (
@@ -13,7 +14,14 @@ const Home: NextPage = () => {
         />
       </Head>
 
+<Header/>      
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <h1 className="text-5xl font-bold hover:underline">
+        Garaikunde
+        </h1>
+        <h1 className="text-5xl font-bold hover:underline">
+        Garaikunde
+        </h1>
         <h1 className="text-5xl font-bold hover:underline">
         Garaikunde
         </h1>
